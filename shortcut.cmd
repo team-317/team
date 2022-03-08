210e75f4c3b780e329fb917d81498cc8f6da88ee
Team317 <2629757717@qq.com>
2022-03-08 15:46:01 +0800

    update

--- shortcut.cmd
+++ shortcut.cmd
@@ -1,5 +1,5 @@
 hugo -F --cleanDestinationDir
-hugo -d --destination ./docs --buildDrafts  --theme=Mainroad
+hugo --destination ./docs --buildDrafts  --theme=Mainroad
 git add .
 git commit -m "update"
 git push
